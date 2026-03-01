e8d5c1705c12e7f7b7abe2d5970cf4257b0c48ea
Salavat <salavat.tukhvatullin1@gmail.com>
2026-03-02 00:35:16 +0300

    Iter5 add function wich get parameters from env

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
 	"runtime"
 	"time"
 
+	"github.com/caarlos0/env"
 	"github.com/tukhvatullinsm/golang-project/internal/handlers/agent"
 	"github.com/tukhvatullinsm/golang-project/internal/metrics"
 )
 
-var WebServer struct {
-	Endpoint string
-}
-
-var WebClient struct {
-	PollInterval   time.Duration
-	ReportInterval time.Duration
+type Config struct {
+	Endpoint       string        `env:"ADDRESS"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
 }
 
 func init() {
-	flag.StringVar(&WebServer.Endpoint, "a", "localhost:8080", "Enter endpoint socket (address:port)")
-	flag.DurationVar(&WebClient.ReportInterval, "r", 10, "Report interval for metrics in seconds")
-	flag.DurationVar(&WebClient.PollInterval, "p", 2, "Poll interval for metrics in seconds")
 
 }
 
@@ -62,6 +59,24 @@ var MetricsName = []string{
 }
 
 func main() {
+	// TODO: Init Client Configuration
+	os.Setenv("ADDRESS", "localhost:443")
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.Endpoint == "" {
+		flag.StringVar(&cfg.Endpoint, "a", "localhost:8080", "Enter endpoint socket (address:port)")
+	}
+	if cfg.ReportInterval == 0 {
+		flag.DurationVar(&cfg.ReportInterval, "r", 10, "Report interval for metrics in seconds")
+	}
+	if cfg.PollInterval == 0 {
+		flag.DurationVar(&cfg.PollInterval, "p", 2, "Poll interval for metrics in seconds")
+	}
+	flag.Parse()
 	// TODO : init runtime memstat object
 	runMemStat := runtime.MemStats{}
 	// TODO: init agent metrics object
@@ -69,12 +84,12 @@ func main() {
 	metricsObj.Init(&runMemStat, MetricsName)
 	// TODO: init agent handler object
 	agentApp := agent.AgentApp{}
-	agentApp.Init(scheme, WebServer.Endpoint, &metricsObj)
+	agentApp.Init(scheme, cfg.Endpoint, &metricsObj)
 	// TODO: run main algorithm
 
 	for {
 		agentApp.UpdateValue()
-		time.Sleep(WebClient.PollInterval * time.Second)
+		time.Sleep(cfg.PollInterval * time.Second)
 		if int64(metricsObj.PollCount)%5 == 0 {
 			agentApp.SendMetric()
 		}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
 
+	"github.com/caarlos0/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/tukhvatullinsm/golang-project/internal/handlers/server"
 	"github.com/tukhvatullinsm/golang-project/internal/storage"
 )
 
-var WebServer struct {
-	Endpoint string
+type Config struct {
+	Endpoint string `env:"ADDRESS"`
 }
 
 func init() {
-	flag.StringVar(&WebServer.Endpoint, "a", "localhost:8080", "Enter endpoint socket (address:port)")
+
 }
 
 func main() {
-	// TODO: Init Server (Endpoint) socket
-	flag.Parse()
+	// TODO: Init Server Configuration (Endpoint)
+	//os.Setenv("ADDRESS", "localhost:100")
+
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if cfg.Endpoint == "" {
+		flag.StringVar(&cfg.Endpoint, "a", "localhost:8080", "Enter endpoint socket (address:port)")
+		flag.Parse()
+	}
+
 	// TODO: init storage object
 	objStorage := storage.New()
 
@@ -34,7 +47,7 @@ func main() {
 	router.Post("/update/{type}/{name}/{value}", webapp.SetValues)
 
 	// TODO: Run and Check Server
-	err := http.ListenAndServe(WebServer.Endpoint, router)
+	err = http.ListenAndServe(cfg.Endpoint, router)
 	if err != nil {
 		panic(err)
 	}
